1d9262f9e40986c6a2a3ad25d22c23b6eb330f38
Andrii Ruda <andrii.ruda@botscrew.com>
2021-01-30 12:43:51 +0200

    Add uploading photos

--- just-chat/Models/User/AppUser.swift
+++ just-chat/Models/User/AppUser.swift
@@ -3,9 +3,11 @@ import Foundation
 struct AppUser: Identifiable, Codable {
     var id: String
     var email: String
+    var imageUrl: String
     
-    init(id: String, email: String) {
+    init(id: String, email: String, imageUrl: String) {
         self.id = id
         self.email = email
+        self.imageUrl = imageUrl
     }
 }
